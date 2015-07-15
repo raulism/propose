0dd6170b6d52a2b49b408d1286e53a749b76ccf3
raulism <raulism@outlook.com>
2015-07-15 22:26:02 +0800

    修复bug

--- js/fire.js
+++ js/fire.js
@@ -21,7 +21,7 @@ function animate() {
 	ctx.restore();
 	var newTime = new Date();
 	if (newTime - lastTime > 500 + (window.innerHeight - 767) / 2) {
-		var random = Math.random() * 100 > 25 ? true: false;
+		var random = Math.random() * 100 > 20 ? true: false;
 		var x = getRandom(canvas.width / 5, canvas.width * 4 / 5);
 		var y = getRandom(50, 200);
 		if (random) {
@@ -39,7 +39,7 @@ function animate() {
 			bigbooms.push(bigboom)
 		}
 		lastTime = newTime;
-		console.log(bigbooms)
+		//console.log(bigbooms)
 	}
 	stars.foreach(function() {
 		this.paint()
